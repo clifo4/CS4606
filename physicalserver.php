967322f33642541a8a497a7627327ea81a60ab96
Jacob Smith <jacobs96@vt.edu>
2018-04-24 20:17:27 -0400

    finish relation scripts

---
+++ physicalserver.php
@@ -0,0 +1,27 @@
+<?php
+$dbconn = pg_connect("user='team sick scripts' dbname='team sick scripts' password =123456") 
+	or die('Could not connect: ' . pg_last_error());
+
+$query = 'SELECT * FROM physicalserver LIMIT 10';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+// Printing results in HTML
+
+echo "<table>\n";
+echo "<tr><b>psID\tpsNumCores\tpsRAM\tpsAdmin</b></tr>";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+	echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Free resultset
+pg_free_result($result);
+
+
+// Closing connection
+pg_close($dbconn);
+
+?>
